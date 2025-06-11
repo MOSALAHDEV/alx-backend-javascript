1cee41ef2277071424cb97bf5e6ef44caeeb3362
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-11 23:16:55 +0000

    add

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to ALX, what is your name?\n', (answer) => {
+  console.log(`Your name is: ${answer}`);
+    rl.close();
+    console.log('This important software is now closing');
+});
