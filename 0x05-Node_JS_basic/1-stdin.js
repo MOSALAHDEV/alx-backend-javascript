431109972bb4895ab37d5768ce2e30481085e85f
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-11 23:59:02 +0000

    add

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,11 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-    process.stdin.on('data', (data) => {
-        process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-        
-        process.exit();
-    });
-} else {
-    process.stdin.on('data', (data) => {
-        process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-        
-        process.exit();
-    });
-}
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
+});
 
 process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
 
