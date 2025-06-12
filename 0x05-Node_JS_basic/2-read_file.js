109c3857e4ac24db48c2f39214a063c308714abb
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-12 01:02:26 +0000

    add

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    
+    const lines = data.split('\n');
+    
+    const students = lines.filter(line => line.trim() !== '').map(line => line.split(','));
+
+    const fields = {};
+
+    students.slice(1).forEach(student => {
+      const [firstName, lastName, age, field] = student;
+      if (fields[field]) {
+        fields[field].push(firstName);
+      } else {
+        fields[field] = [firstName];
+      }
+    });
+
+    console.log(`Number of students: ${students.length - 1}`);
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+    
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
+
