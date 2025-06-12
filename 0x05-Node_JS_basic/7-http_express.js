c1f05564799e656cd12f52da48f5b4eae520a0e5
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-12 02:06:12 +0000

    add

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const { readFile } = require('fs');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 1245;
 
+// Function to count students from the CSV file
 function countStudents(fileName) {
   const students = {};
   const fields = {};
   let length = 0;
+
   return new Promise((resolve, reject) => {
-    readFile(fileName, (err, data) => {
+    fs.readFile(fileName, (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
         let output = '';
         const lines = data.toString().split('\n');
         
-        // Loop over lines to process students
-        for (let i = 1; i < lines.length; i += 1) { // Skip header
+        // Skip the header and loop through lines to process students
+        for (let i = 1; i < lines.length; i++) {
           if (lines[i]) {
             length += 1;
             const field = lines[i].toString().split(',');
@@ -40,8 +43,8 @@ function countStudents(fileName) {
           }
         }
 
-        const l = length;
-        output += `Number of students: ${l}\n`;
+        const totalStudents = length;
+        output += `Number of students: ${totalStudents}\n`;
 
         // Format output for each field
         for (const [key, value] of Object.entries(fields)) {
@@ -50,33 +53,47 @@ function countStudents(fileName) {
             output += `List: ${students[key].join(', ')}\n`;
           }
         }
+
         resolve(output);
       }
     });
   });
 }
 
+// Route for the root path
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Route for the /students path
 app.get('/students', (req, res) => {
-  const dbPath = req.query.db;
+  const dbPath = req.query.db; // Fetch the database file from the query parameter
 
   if (!dbPath) {
     res.status(400).send('Database path is required');
     return;
   }
 
-  countStudents(dbPath)
-    .then((output) => {
-      res.send(['This is the list of our students', output].join('\n'));
-    })
-    .catch((err) => {
-      res.status(500).send('This is the list of our students\n' + err.message);
-    });
+  // Check if file exists
+  const filePath = path.resolve(dbPath);
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.status(500).send('Cannot load the database');
+      return;
+    }
+
+    // Call the function to count students from the file
+    countStudents(filePath)
+      .then((output) => {
+        res.send(['This is the list of our students', output].join('\n'));
+      })
+      .catch((error) => {
+        res.status(500).send('This is the list of our students\n' + error.message);
+      });
+  });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
