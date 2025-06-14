d691eeac7c3b63e311788fa3f3b2abd1ea92bdfc
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-14 12:11:46 +0000

    add

--- 0x05-Node_JS_basic/.eslintrc.js
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -1,26 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'script'
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-tabs': 'error',
+    'space-before-blocks': ['error', 'always']
+  }
 };
+
